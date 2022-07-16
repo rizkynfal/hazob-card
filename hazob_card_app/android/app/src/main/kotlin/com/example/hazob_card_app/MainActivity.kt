329ebf618e063cb1efb0ff2dc0369ce98eeea34a
Rizky Naufal <naufalrizky79@gmail.com>
2022-07-16 09:06:34 +0700

    INIT COMMIT

---
+++ hazob_card_app/android/app/src/main/kotlin/com/example/hazob_card_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hazob_card_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hazob_card_app/lib/routes/routes.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+String SPLASH_SCREEN = '/SplashScreen',
+    HOME_SCREEN = '/HomeScreen',
+    PHOTO_CONTAINER_SCREEN = '/PhotoContainerScreen',
+    VIDEO_CONTAINER_SCREEN = '/VideoContainerScreen',
+    ALBUM_CONTAINER_SCREEN = '/AlbumContainerScreen',
+    MESSAGE_VIEW = 'MessageView';
+
+Color mainColor = const Color(0xFFFFE5B4);
+Color fontMainColor = const Color(0xFF3A5BA0);
+Color darkOrangeColor = const Color(0xFFFFA500);
+Color darkBlueColor = const Color(0xFF1F4690);
+Color warningColor = const Color(0xFFF6C000);
+Color dangerColor = const Color(0xFFFF6969);
